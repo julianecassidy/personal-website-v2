a1a62c6ba57c66fefefbba474ae3543e7bc480d6
Juliane <jcassidy@cleverbenefits.com>
2025-05-07 12:55:47 -0600

    install prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    tabWidth: 2,
+    singleQuote: true,
+    parser: 'typescript',
+    printWidth: 100,
+    bracketSpacing: false,
+    trailingComma: 'all',
+    endOfLine: 'auto',
+};
